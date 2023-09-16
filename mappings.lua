acad66f89b9c0b58410e351a6e83fd5e6013d934
Deek <deekcrypto@gmail.com>
2023-09-16 15:38:18 -0600

    toggleterm mapping

--- mappings.lua
+++ mappings.lua
@@ -30,11 +30,13 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<C-t>"] = { "<cmd>ToggleTerm direction=float<cr>" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+    ["<C-t>"] = { "<cmd>ToggleTerm direction=float<cr>" },
   },
 }
